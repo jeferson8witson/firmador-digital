2f3cd08d570473ab6c3c08a6693597e70c60df9f
Jhrnandez <juan@brandpetram.com>
2022-04-22 09:17:51 -0500

    ignorar el usuario 1

--- pages/admin/Table.js
+++ pages/admin/Table.js
@@ -1,86 +1,116 @@
 import { useState, useEffect } from "react";
 import firebase from "../../components/firebase";
 const people = [
-    { name: 'Jane Cooper', title: 'Regional Paradigm Technician', role: 'Admin', email: 'jane.cooper@example.com' },
-    { name: 'Cody Fisher', title: 'Product Directives Officer', role: 'Owner', email: 'cody.fisher@example.com' },
-    // More people...
-  ]
-  
- const Table = () => {
-    const [activeCursos, setActiveCursos] = useState([]);
-  
-    useEffect(() => {
-      const cursosRef = firebase
-        .firestore()
-        .collection("registros")
-        .doc("registros");
-      cursosRef.onSnapshot((snapshot) => {
-        setActiveCursos(snapshot.data().usuario.reverse());
-        console.log(snapshot.data().usuario)
-      });
-    }, []);
-    return (
-      <div className="flex flex-col">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+  {
+    name: "Jane Cooper",
+    title: "Regional Paradigm Technician",
+    role: "Admin",
+    email: "jane.cooper@example.com",
+  },
+  {
+    name: "Cody Fisher",
+    title: "Product Directives Officer",
+    role: "Owner",
+    email: "cody.fisher@example.com",
+  },
+  // More people...
+];
+
+const Table = () => {
+  const [activeCursos, setActiveCursos] = useState([]);
+
+  useEffect(() => {
+    const cursosRef = firebase
+      .firestore()
+      .collection("registros")
+      .doc("registros");
+    cursosRef.onSnapshot((snapshot) => {
+      setActiveCursos(snapshot.data().usuario.reverse());
+      console.log(snapshot.data().usuario);
+    });
+  }, []);
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Usuario
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Contraseña
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Metodo
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Entidad
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Registros
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {activeCursos.map((person, personIdx) =>
+                  personIdx > 0 ? (
+                    <tr
+                      key={person.email}
+                      className={
+                        personIdx % 2 === 0 ? "bg-white" : "bg-gray-50"
+                      }
                     >
-                      Usuario
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Contraseña
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Metodo
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Entidad
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Registros
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {activeCursos.map((person, personIdx) => (
-                    <tr key={person.email} className={personIdx % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{person.user}</td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.password}</td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.metodo}</td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.entidad}</td>
-                      <td className="whitespace-nowrap "><a href={"/admin/registros?usuario=" + personIdx}><button className="py-2 px-2 bg-blue-500 text-white font-bold rounded"> Ver registros</button></a></td>
-                      {person.keys ? 
-                    <>
-                      
-                    </>
-                    :""}
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {person.user}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {person.password}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {person.metodo}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {person.entidad}
+                      </td>
+                      <td className="whitespace-nowrap ">
+                        <a href={"/admin/registros?usuario=" + personIdx}>
+                          <button className="py-2 px-2 bg-blue-500 text-white font-bold rounded">
+                            {" "}
+                            Ver registros
+                          </button>
+                        </a>
+                      </td>
+                      {person.keys ? <></> : ""}
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                  ) : (
+                    ""
+                  )
+                )}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-    )
-  }
-  
-  export default Table;
+    </div>
+  );
+};
+
+export default Table;
