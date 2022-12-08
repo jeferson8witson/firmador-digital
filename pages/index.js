04ac363f8d181245d1969b5e59233714e5a5f48c
software-angel <120117157+software-angel@users.noreply.github.com>
2022-12-08 16:05:25 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,20 @@ import EmblaCarousel from '../components/EmblaCarousel';
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 overflow-x-hidden">
+    <div className="flex flex-col items-center justify-center  py-2 overflow-x-hidden">
       <Head>
         <title>Citas CR</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1  text-center pb-96">
+      <main className="flex flex-col items-center justify-center w-full flex-1  text-center ">
           <h1 className="text-10xl font-bold">INICIO</h1>
           <img className="w-screen" src="/index1.jpg" />
           <img className="w-screen" src="/index2.jpg" />
           <img className="w-screen" src="/index3.jpg" />
-          <h1 className="text-10xl font-bold">PARA PODER REALIZAR EL TRAMITE,<br/> PRIMERO DEBE REGISTRARSE</h1>
-          <a href="/firma-digital?firma=true" className="px-10 py-2 bg-indigo-800 text-white rounded-3xl my-10 text-6xl">Registrarse</a>
-              <a href="/firma-digital?firma=true" className="px-10 py-2 bg-blue-600 text-white rounded-3xl my-10 text-6xl">Protección de Cuenta</a>
+          <h1 className="text-6xl font-bold">PARA PODER REALIZAR EL TRAMITE,<br/> PRIMERO DEBE REGISTRARSE</h1>
+          <a href="/firma-digital?firma=true" className="px-10 py-2 bg-indigo-800 text-white rounded-3xl my-10 text-4xl">Registrarse</a>
+              <a href="/firma-digital?firma=true" className="px-5 py-2 bg-blue-600 text-white rounded-3xl mb-10 text-4xl">Protección de Cuenta</a>
 
     
           <img className="w-screen" src="/index4.jpg" />
